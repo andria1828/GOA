90ce53d292222b072303e109cdb56e7701edff16
andria1828 <andriakobakhidze615@gmail.com>
2024-05-23 22:38:23 +0400

    first commit

---
+++ day 1/homework/lesson1.py
@@ -0,0 +1,31 @@
+name= "andria"
+
+
+surname= "kobakhidze"
+
+print(name)
+
+
+
+
+name ="andria"
+age =  12
+heigth = 159
+
+
+knows_programing = True
+is_ugly =False
+
+
+
+
+print(name +""+ surname)
+
+
+
+
+print(name + " " +  str(age))
+
+has_pet=True
+
+lives_in_tbilisi=True
