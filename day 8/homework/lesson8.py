46c69ae5f3331182cc7153fdd442194fe13d6f21
andria1828 <andriakobakhidze615@gmail.com>
2024-05-28 12:24:17 +0400

    second commit

---
+++ day 8/homework/lesson8.py
@@ -0,0 +1,65 @@
+# == მაგალითები
+
+
+print(5 == 5)
+print(5 == 8)
+print(2 == 1)
+print(9 == 9)
+
+# > მაგალითბი
+
+print(5 > 3)
+print(4 > 4)
+print(9 > 10)
+print(6 > 7)
+print(2 > 1)
+
+# < მაგალითები
+
+print(2 < 3)
+print(3 < 2)
+print(1 < 5)
+print(7 < 3)
+print(2 < 1)
+
+# >= მაგალითბი
+
+print(5 >= 5)
+print(1 >= 0)
+print(2 >= 2)
+print(8 >= 7)
+print(3 >= 2)
+
+# <= მაგალითები
+
+print(3 <= 2)
+print(3 <= 3)
+print(7 <= 8)
+print(5 <= 5)
+print(2 <= 1)
+
+# != მაგალითები
+
+print(2 != 2)
+print()
+print()
+print()
+print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
