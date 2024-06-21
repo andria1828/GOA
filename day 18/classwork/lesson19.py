a47fe5340e250843a09ed87029e8e427dd4ad72c
wiklauri27giorgi <gtsiklauri27@gmail.com>
2024-06-21 09:56:27 +0400

    second commit

---
+++ day 18/classwork/lesson19.py
@@ -0,0 +1,16 @@
+
+numbers_list = []
+
+for num in range(1, 11):
+    numbers_list.append(num)
+
+list_length = len(numbers_list)
+
+print("Numbers list:", numbers_list)
+print("Length of the list:", list_length)
+
+
+
+
+
+
