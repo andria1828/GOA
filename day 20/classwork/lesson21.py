d8b77b84a1a5aca175a8732861966a0b6ac2fc15
andria1828 <andriakobakhidze615@gmail.com>
2024-06-27 22:21:46 +0400

    second commit

---
+++ day 20/classwork/lesson21.py
@@ -0,0 +1,11 @@
+def display_info(first_name, last_name, age):
+    print("First Name:", first_name)
+    print("Last Name:", last_name)
+    print("Age:", age)
+
+# Example usage:
+first_name = "andria"
+last_name = "kobakhidze"
+age = 13
+
+display_info(first_name, last_name, age)
